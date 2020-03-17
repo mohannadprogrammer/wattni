e8c16cfe0c17146734bcdff95564151708fa00a1
waheedmohannad55578@gmail.com <waheedmohannad55578@gmail.com>
2020-03-17 07:45:23 -0700

    Revert "make git massageCard reusable "
    This reverts commit 895625bf00d3732319e84660507ec6a63299bd91.

--- src/Views/test.js
+++ src/Views/test.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import {
-  MassageCard
+
 }from '../component'
 // import { from } from 'rxjs'
 export default class test extends Component {
   render() {
     return (
       <View>
-        <MassageCard/>
+        <Text> textInComponent </Text>
       </View>
     )
   }
--- src/component/MassageCard/massageCard.js
+++ src/component/MassageCard/massageCard.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, {Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
 
 export default class MassageCard extends Component {
   render() {
     return (
       <View style={styles.box}>
-        <View style={styles.position}>
-          <Text>{this.props.title}</Text>
-        </View>
+        <View style={styles.position}></View>
         {this.props.children}
       </View>
     );
   }
 }
-const colors = require("../../assest/colors")
+const colors = require ("../../assest/colors")
 const styles = StyleSheet.create({
   box: {
     borderWidth: 2,
@@ -32,10 +30,8 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 0,
     left: 0,
-    alignItems: "center",
     height: 30,
-    // width: 50,
-    paddingHorizontal: 20,
+    width: 30,
     backgroundColor: colors.green_color2,
     borderBottomRightRadius: 10,
   },
