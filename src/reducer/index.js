cc82881ac8dafa803e89e4a426a481b3c468223b
waheedmohannad55578@gmail.com <waheedmohannad55578@gmail.com>
2020-03-25 06:09:20 -0700

    import in the mai sat

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -4,10 +4,12 @@ import
     Product
  from './MainProduct'
  import regist from './registStore'
+ import cart from './Cart'
 
 const rootReducer = combineReducers({
     Product,
     regist
+    ,cart
 })
 
 export default rootReducer;
